3c9b5b25a1192134c0b43658ccd087f655a00c2b
Vaibhav Tiwari <tiwarijivaibhav@gmail.com>
2025-01-15 02:59:21 +0530

    strings

--- Program.java
+++ Program.java
@@ -1,19 +1,14 @@
 public class Program {
     public static void main(String args[]){
 
-        int arr[][]=new int[4][4];
 
-        arr[0][0]=1;
-        arr[1][0]=2;
+        StringBuilder a=new StringBuilder("abc");
+
+        System.out.println(a);
+
         
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                System.out.print(arr[i][j]+" ");
-
-            }
-            System.out.println();
-            
-        }
+        
+        
 
     }
     
